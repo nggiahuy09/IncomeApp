7b78dc09c638d93b98801652dd30511394456977
Gia Huy Nguyen <nggiahuy09@gmail.com>
2025-12-21 13:07:35 +0700

    feat (home): impl the UI of transaction item

--- IncomeApp/HomeView.swift
+++ IncomeApp/HomeView.swift
@@ -17,7 +17,33 @@ struct HomeView: View {
         VStack {
             List {
                 ForEach(transactions, content: {transaction in
-                    Text(transaction.title)
+                    VStack {
+                        HStack(spacing: 12.0) {
+                            Image(systemName: transaction.transactionType == .income ? "arrow.up.forward" : "arrow.down.forward")
+                                .font(.system(size: 16.0, weight: .bold))
+                                .foregroundStyle(transaction.transactionType == .income ? .green : .red)
+                            VStack(alignment: .leading, spacing: 8.0) {
+                                HStack {
+                                    Text(transaction.title)
+                                        .font(.system(size: 16.0, weight: .bold))
+                                    Spacer()
+                                    Text(transaction.displayAmount)
+                                        .font(.system(size: 15.0, weight: .bold))
+                                }
+                                HStack {
+                                    Text("Completed")
+                                        .font(.system(size: 14.0))
+                                    Spacer()
+                                    Text(transaction.displayDate)
+                                        .font(.system(size: 14.0))
+                                        .padding(.vertical, 4.0)
+                                        .padding(.horizontal, 8.0)
+                                        .background(Color.lightGradeShade.opacity(0.5))
+                                        .clipShape(RoundedRectangle(cornerRadius: 8.0))
+                                }
+                            }
+                        }
+                    }
                 })
             }
         }
--- IncomeApp/Models/TransactionModel.swift
+++ IncomeApp/Models/TransactionModel.swift
@@ -13,4 +13,21 @@ struct TransactionModel: Identifiable {
     let transactionType: TransactionType
     let amount: Double
     let date: Date
+    
+    var displayDate: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        
+        return dateFormatter.string(from: date)
+    }
+    
+    var displayAmount: String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        //        numberFormatter.locale = .current
+        numberFormatter.maximumFractionDigits = 2
+        
+        return numberFormatter.string(from: amount as NSNumber) ?? "N/A"
+    }
 }
