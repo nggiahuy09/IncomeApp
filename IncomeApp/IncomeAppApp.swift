1ecf546d13fb8a39c39faf1edd5366f83d352c87
Gia Huy Nguyen <nggiahuy09@gmail.com>
2025-12-21 12:12:55 +0700

    Initial Commit

---
+++ IncomeApp/IncomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeAppApp.swift
+//  IncomeApp
+//
+//  Created by Nguyễn Gia Huy on 21/12/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
