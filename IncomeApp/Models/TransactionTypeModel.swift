3485697c5892510e29eec7a4c9182bdf3e231711
Gia Huy Nguyen <nggiahuy09@gmail.com>
2025-12-21 20:51:31 +0700

    feat (add transaction): fully impl the UI of the add transaction view

--- IncomeApp/Models/TransactionTypeModel.swift
+++ IncomeApp/Models/TransactionTypeModel.swift
@@ -7,4 +7,17 @@
 
 import Foundation
 
-enum TransactionType { case income, expense }
+enum TransactionType: String, CaseIterable, Identifiable {
+    case income, expense
+
+    var id: Self { self }
+
+    var title: String {
+        switch self {
+        case .expense:
+            return "Expense"
+        case .income:
+            return "Income"
+        }
+    }
+}
--- IncomeApp/Views/AddTransactionView.swift
+++ IncomeApp/Views/AddTransactionView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct AddTransactionView: View {
+
+    @State private var amount: Double = 0.0
+    @State private var transactionTitle: String = ""
+    @State private var selectedTransactionType: TransactionType = .expense
+
+    var numberFormatter: NumberFormatter {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        return numberFormatter
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 24.0) {
+            TextField("0.00", value: $amount, formatter: numberFormatter)
+                .font(.system(size: 32.0, weight: .thin))
+                .multilineTextAlignment(.center)
+                .keyboardType(.numberPad)
+            Divider()
+            HStack {
+                Text("Choose Type: ")
+                Spacer()
+                Picker("Choose Type", selection: $selectedTransactionType, content: {
+                    ForEach(TransactionType.allCases, content: { type in
+                        Text(type.title)
+                            .tag(type)
+                    })
+                })
+            }
+            TextField("Title", text: $transactionTitle)
+                .font(.system(size: 16.0))
+                .textFieldStyle(.roundedBorder)
+            Button(action: {}, label: {
+                Text("Create")
+                    .font(.system(size: 16.0, weight: .semibold))
+                    .foregroundStyle(.white)
+                    .frame(height: 40.0)
+                    .frame(maxWidth: .infinity)
+                    .background(.primaryLightGreen)
+                    .clipShape(RoundedRectangle(cornerRadius: 8.0))
+
+            })
+            .padding(.top, 48.0)
+            Spacer()
+        }
+        .padding(.horizontal)
+        .padding(.top, 32.0)
     }
 }
 
